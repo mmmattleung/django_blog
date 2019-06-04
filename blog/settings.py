1dd7b7ad3bd5c32fd18c22e002f30cd1d797d708
Unknown <mattleungcanton@outlook.com>
2019-06-04 14:56:53 +0800

    update

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'repository',
     'api',
+    # 'rbac_permission',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'middlewares.middle_error.M1',
+    'middlewares.middle_error.M1',
 ]
 
 ROOT_URLCONF = 'blog.urls'
