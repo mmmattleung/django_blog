560eaf4887f0b4f3c29501533f07ce1d48843352
Unknown <mattleungcanton@outlook.com>
2019-06-04 16:06:06 +0800

    update

--- blog/settings.py
+++ blog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middlewares.middle_error.M1',
+    # 'middlewares.middle_error.M1',
 ]
 
 ROOT_URLCONF = 'blog.urls'
