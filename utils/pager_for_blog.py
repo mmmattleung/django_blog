07960dd96f3e8e791f799b2a05f4cd663d01a019
Matt_Leung <mattleungcanton@outlook.com>
2019-08-20 23:24:31 +0800

    update

--- utils/pager_for_blog.py
+++ utils/pager_for_blog.py
@@ -11,18 +11,20 @@ def get_pager(request, articles, pages, reverse_name, **kwargs):
     p = Paginator(articles, pages)
     ps = p.page(current_page)
 
-    if current_page - pages<= 0:
-        begin= 1
-        if p.num_pages < pages:
+
+    print(current_page, pages, p.num_pages)
+    if current_page - pages <= 0:
+        begin = 1
+        if p.num_pages <= pages:
             end = p.num_pages + 1
         else:
             end = pages * 2 + 1
     elif current_page + pages > p.num_pages:
         print("elif")
-        begin= p.num_pages - (pages * 2 + 1)
+        begin = p.num_pages - (pages * 2 + 1)
         end = p.num_pages
     else:
-        begin= current_page - pages
+        begin = current_page - pages
         end = current_page + pages + 1
 
     diy_range = range(begin, end)
